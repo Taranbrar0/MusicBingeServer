a11e898ee4687d749b88e67855176fd80cb6819b
Taranbrar0 <112814527+Taranbrar0@users.noreply.github.com>
2024-07-31 20:02:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const corsOptions ={
-    origin: 'http://localhost:3000',
+    origin: 'https://musicbinge.netlify.app/',
     optionsSuccessStatus:200
 }
 
